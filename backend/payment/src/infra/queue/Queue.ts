96f69c7564ba2402e7dc85222d62fa48e1bf8a07
Rodrigo Branas <rodrigo.branas@gmail.com>
2024-10-14 09:48:41 -0300

    updated

--- backend/payment/src/infra/queue/Queue.ts
+++ backend/payment/src/infra/queue/Queue.ts
@@ -16,11 +16,25 @@ export class RabbitMQAdapter implements Queue {
 
 	async publish(exchange: string, data: any): Promise<void> {
 		const channel = await this.connection.createChannel();
+		await channel.assertExchange("rideCompleted", "direct", { durable: true });
+		await channel.assertQueue("rideCompleted.processPayment", { durable: true });
+		await channel.assertQueue("rideCompleted.generateInvoice", { durable: true });
+		await channel.assertQueue("rideCompleted.sendReceipt", { durable: true });
+		await channel.bindQueue("rideCompleted.processPayment", "rideCompleted", "");
+		await channel.bindQueue("rideCompleted.generateInvoice", "rideCompleted", "");
+		await channel.bindQueue("rideCompleted.sendReceipt", "rideCompleted", "");
 		channel.publish(exchange, "", Buffer.from(JSON.stringify(data)));
 	}
 
 	async consume(queue: string, callback: Function): Promise<void> {
-		const channel = await this.connection.createChannel()
+		const channel = await this.connection.createChannel();
+		await channel.assertExchange("rideCompleted", "direct", { durable: true });
+		await channel.assertQueue("rideCompleted.processPayment", { durable: true });
+		await channel.assertQueue("rideCompleted.generateInvoice", { durable: true });
+		await channel.assertQueue("rideCompleted.sendReceipt", { durable: true });
+		await channel.bindQueue("rideCompleted.processPayment", "rideCompleted", "");
+		await channel.bindQueue("rideCompleted.generateInvoice", "rideCompleted", "");
+		await channel.bindQueue("rideCompleted.sendReceipt", "rideCompleted", "");
 		channel.consume(queue, async function (message: any) {
 			const input = JSON.parse(message.content.toString());
 			await callback(input);
